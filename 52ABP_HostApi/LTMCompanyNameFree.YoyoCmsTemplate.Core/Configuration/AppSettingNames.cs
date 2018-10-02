b1b900c9fd557d994d87451ed8362f11501df9a8
桐铭 梁 <werltm@hotmail.com>
2018-10-02 16:56:14 +0800

    移除

--- 52ABP_HostApi/LTMCompanyNameFree.YoyoCmsTemplate.Core/Configuration/AppSettingNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LTMCompanyNameFree.YoyoCmsTemplate.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-    }
-}
--- AdminUI/ClientApp/src/shared/component-base/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app-component-base';
-export * from './modal-component-base';
-export * from './paged-listing-component-base';
-export * from './modal-paged-listing-component-base';
